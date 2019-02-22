57b8ea6f5ea8a8f385a696cac8da86bbdecfefe3
yueyue12958 <jiayueyu@usc.edu>
2019-02-22 12:28:13 -0800

    Update Image check

--- Image_check.py
+++ Image_check.py
@@ -1,22 +1,23 @@
 import matplotlib.image as mpimg
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
-import os  
-counter=0
-for i in range (1,34): # for Tianjiao, use this line
-# for i in range (34,67): # for Xin, use this line
-# for i in range (67,101): # for Jiayue, use this line
-    #print("i:"+str(i)) #uncomment this line to check which image is not readable, if so, change a new image
-    for j in range (1,6):
-        # Read image 
-        #print(j) #uncomment this line to check which image is not readable, if so, change a new image
-        path='/Users/yueyue/Desktop/usc/M2S2/EE599/project/train/'+str('{:03}'.format(i))+'/'+str(j)+'.png'
+import os
+from skimage import transform,data
+from skimage import color
+np_fashion_train = np.zeros([6,784])
+#print(len(result))
+for i in range (39,41):
+    for j in range (1,4):
+        # Read image
+        path='/Users/yueyue/Desktop/usc/M2S2/EE599/project/new_train/'+str('{:03}'.format(i))+'/'+str(j)+'.png'
         if(os.path.isfile(path)==0):
             path='/Users/yueyue/Desktop/usc/M2S2/EE599/project/train/'+str('{:03}'.format(i))+'/'+str(j)+'.jpg'
-        img = mpimg.imread(path) 
+        img = mpimg.imread(path)
+        img = color.rgb2gray(transform.resize(img, (28, 28))) #resize
         # Output Images (uncomment below two lines to check if image can be printed sucessfully)
-        #plt.figure(i*5+j)
-        #imgplot = plt.imshow(img)
-        counter+=1
-print(counter) # please check final print counter==33*5
-
+        plt.figure(i*3+j)
+        imgplot = plt.imshow(img, cmap='gray')
+        plt.show()
+        img=img.flatten()
+    np_fashion_train[(i-39)*3+j-1]=img
+#np.savetxt("/Users/yueyue/Desktop/usc/M2S2/EE599/project/new_train/new.csv", result, delimiter=',')
